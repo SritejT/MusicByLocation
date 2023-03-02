e30917fc8ea770dc0e4a913d6b44d7a46694ab0f
Sritej Tummuru <sritej.tummuru@gmail.com>
2023-03-02 14:48:26 +0000

    Initial Commit

---
+++ MusicByLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicByLocation
+//
+//  Created by Ravindra Tummuru on 02/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
